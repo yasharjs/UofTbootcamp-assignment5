58996833b1a89f404dfcdc3229c868daad7c50e6
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2021-12-06 23:34:27 -0500

    added save feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var blockEventsList = [];
 
-// get and format the currentDay and display it on the page
+//format the currentDay and display it on the page
 var displayCurrentDay = function(){
     var tempText = moment().format("dddd, MMMM Do");
     $("#currentDay").text(tempText);
@@ -17,54 +17,59 @@ var displayBlockEvents = function(){
         .text(moment(i,"H")
         .format("ha"));
 
+
         var tempId = i - 7;
-        tempTextarea = $("<textarea>").addClass("col past").attr("id",tempId);
+
+        if(blockEventsList[tempId]){
+            var tempText = blockEventsList[tempId];
+        }
+        else{
+            var tempText = "";
+        }
+        
+        tempTextarea = $("<textarea>").addClass("col past").attr("id",tempId).text(tempText);
+        
 
         tempButtonDiv = $("<div>")
-        .addClass("col-1 saveBtn")
+        .addClass("col-2 col-md-1 saveBtn")
         .append($("<span>")
         .addClass("oi oi-circle-check"));
 
         tempRow.append(tempTimeOfDay,tempTextarea,tempButtonDiv);
-        blockEventsList.push(tempRow);
         $(".container").append(tempRow);
     }
-    console.log(blockEventsList);
     
 }
 
-var addSavedEvents = function(){
 
-}
 var loadBlockEvents = function(){
     var tempList = JSON.parse(localStorage.getItem("events"));
 
     //events not empty
     if(tempList){
-        addSavedEvents(tempList);
+        blockEventsList = tempList;
     }
     
 }
 
+
 var loadPage = function(){
+    loadBlockEvents();
     displayCurrentDay();
     displayBlockEvents();
-    loadBlockEvents();
+  
 
 }
 
-$(".container").on("click","textarea",function(){
-  
-  console.log($(this).attr("id"));
-    
-})
-
-$(".container").on("blur","textarea",function(){
-    console.log("blur");
-})
-
-
-
-
-
 loadPage();
+
+
+$(".saveBtn").on("click",function(){
+    var tempText = $(this).siblings("textarea").val();
+    var tempId = $(this).siblings("textarea").attr("id");
+
+    blockEventsList[tempId] = tempText;
+
+    localStorage.setItem("events", JSON.stringify(blockEventsList));
+
+  });
